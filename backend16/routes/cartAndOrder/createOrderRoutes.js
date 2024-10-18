d172ce2c548d0ebacd14bbcdb40ea51c23060023
Muhammad Mubarak <139914643+Mubarak150@users.noreply.github.com>
2024-10-18 20:25:29 +0500

    Delete backend16 directory and others

--- backend16/routes/cartAndOrder/createOrderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { placeOrder } = require('../../controllers/cartAndOrder/createOrderController');
-const {protect} = require('../../middleware/auth')
-
-// order Routes
-router.post('/', protect, placeOrder);
-
-module.exports = router;
