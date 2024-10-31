3e0cfa9f74de7610fc0f690053ac66349e7d1b71
Mubarak150 <mkmubarak2347@gmail.com>
2024-10-31 22:41:46 +0500

    ready to process return of product(s) while not changing the actual sale.

--- routes/POS-v2/returnSoldProductRoutes.js
+++ routes/POS-v2/returnSoldProductRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getSaleBySSN } = require('../../controllers/POS-v2/returnSoldProduct');
+const { getSaleBySSN, processReturn } = require('../../controllers/POS-v2/returnSoldProduct');
 const {protect, isSalesMan} = require('../../middleware/auth')
 const router = express.Router();
 
 router.post('/:ssn', protect, isSalesMan, getSaleBySSN); 
 
+router.post('/ammend/process', protect, isSalesMan, processReturn); 
 module.exports = router; 
