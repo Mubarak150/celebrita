068e7a868f2579e00be6705b81dd77e661e70d72
Mubarak150 <mkmubarak2347@gmail.com>
2024-11-05 17:55:19 +0500

    getuserbasedonroleroutesallowedforadmins

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { signIn, register, logout, forgotPassword, verifyOTP, resetPassword, updateMe, getUsersbyRole} = require('../controllers/authController');
-const {protect, checkSignIn} = require('../middleware/auth');
+const {protect, isUserAdmin, checkSignIn} = require('../middleware/auth');
 const router = express.Router();
 
 // for dev: all are defined in authController... 
@@ -10,7 +10,9 @@ router.post('/register',  register);
 router.post('/logout',  logout);
 router.post('/forgot-password',  forgotPassword);
 router.post('/verify-otp',  verifyOTP);
+
+// user routes.. should have a separate controller and ancilliaries thereto. 
 router.post('/reset-password',  resetPassword);
 router.patch('/update-profile', protect,  updateMe);
-router.get('/users',   getUsersbyRole);
+router.get('/users', protect, isUserAdmin, getUsersbyRole);
 module.exports = router;
